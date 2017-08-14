b99dc8433c6e411478534cac9c44668413540ca3
Dinyo Dinev <dinyo.dinev@paysafe.com>
2017-08-14 19:54:43 +0300

    Add swagger2

--- src/main/java/com/frankmoley/services/room/roomservices/RoomController.java
+++ src/main/java/com/frankmoley/services/room/roomservices/RoomController.java
@@ -1,5 +1,7 @@
 package com.frankmoley.services.room.roomservices;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/rooms")
+@Api(value = "rooms",description = "Data", tags ="rooms")
 public class RoomController {
 
     @Autowired
     RoomRepository roomRepository;
 
     @RequestMapping(method = RequestMethod.GET)
+    @ApiOperation(value = "Get All rooms", notes = "Get all rooms in the system", nickname = "getRooms")
     public List<Room> findAll(@RequestParam(name = "rootNumber",required = false) String roomNumber){
         if(StringUtils.isNoneEmpty(roomNumber)){
             return Collections.singletonList(this.roomRepository.findByRoomNumber(roomNumber));
